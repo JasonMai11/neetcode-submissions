e2018d404ff81f539f62334fc255c0338a4f6042
Jason Mai <86985751+JasonMai11@users.noreply.github.com>
2026-08-31 19:32:02 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.0
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "string"
+print(type(variable))
+variable = [0, 1]
+print(type(variable))
